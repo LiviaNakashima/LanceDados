391621c15b127b66e894af2732ed44f1dff1f370
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-10-24 09:53:52 -0300

    Atualizando o jogo Lançar Dados - imagens randômicas ao clicar no botão

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -12,14 +12,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
 
          botaoLancarDados.setOnClickListener {
+            dado1.setImageResource(lancarDados())
+            dado2.setImageResource(lancarDados())
 
          }
     }
 
     fun lancarDados():Int{
+        return recuperaFaceDado(gerarNumero())
+    }
+
+    fun recuperaFaceDado(valor: Int ):Int {
+        return when(valor){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    fun gerarNumero(): Int{
         return (1..6).random()
     }
 
